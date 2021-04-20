fbd822280bbbae18bf61f590fa77ec8ae40bf8b2
Curtis H <sitrucrettah1@gmail.com>
2021-04-19 20:46:58 -0700

    deploying

--- server.js
+++ server.js
@@ -16,7 +16,15 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/users");
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/users',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // Start the API server
 app.listen(PORT, function() {
